34a29532c68951e6f0664af30cab45f0059ff3f5
Arthur Correa Marques <arthurperfilprof@hotmail.com>
2023-09-13 00:23:45 -0300

    integração da api, puxando os pokemons com um for para puxar toda a listagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,68 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function (response) {
-    console.log(response);
-})
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
 
-const x = 10 + 20
-console.log(x)
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+    // forma reduzida de se fazer, se eu tenho somente 1 "return" posso puxar de forma direta igual foi feito com a function
+fetch(url)
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+        }
+    })
+    .catch((error) => console.error(error))
+
+    // .finally(() => console.log('Requisição Concluída!'))  - Pra validar se deu certo
+
+    // 1 - Modo padrão de fazer sem encurtamentos
+    // .then(function (response) {
+    //     return response.json();
+    // })
+    // .then(function (jsonBody) {
+    //     console.log(jsonBody);
+    // })
+    // .catch(function (error) {
+    //     console.error(error);
+    // })
+    // .finally(function () {
+    //     console.log('Requisição Concluída!');
+    // })
+
+
+    // 2 - Modo de fazer, encurtando somente o chamado de function
+    // .then((response) => {
+    //     return response.json();
+    // })
+    // .then((jsonBody) => {
+    //     console.log(jsonBody);
+    // })
+    // .catch((error) => {
+    //     console.error(error);
+    // })
+    // .finally(() => {
+    //     console.log('Requisição Concluída!');
+    // })
+
+
