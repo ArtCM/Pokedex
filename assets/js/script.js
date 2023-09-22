ee5f90da1e65affd06e5734dc52af71efbcc8bca
Arthur Correa Marques <arthurperfilprof@hotmail.com>
2023-09-22 19:29:18 -0300

    Pausa por conta do bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,12 @@ let offset = 0;
 
 const maxRecords = 151
 
+
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
-                <li class="pokemon ${pokemon.type}">
+                <li class="pokemon pokeclick ${pokemon.type}">
                     <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
 
@@ -25,20 +27,50 @@ function loadPokemonItens(offset, limit) {
     })
 }
 
+function carregarPokemon() {
+    const pokemonElements = document.querySelectorAll('.pokeclick');
+    pokemonElements.forEach((element) => {
+      element.addEventListener('click', () => {
+        const pokemonName = element.querySelector('.name').textContent;
+        const pokemonNumber = element.querySelector('.number').textContent;
+        const pokemonType = element.querySelector('.type').textContent;
+        const pokemonImage = element.querySelector('.pokephoto').src;
+  
+        const pokemonStatus = document.querySelector('.appearpoke');
+        const statusContent = `
+          <h2>${pokemonName}</h2>
+          <p>Número: ${pokemonNumber}</p>
+          <p>Tipo: ${pokemonType}</p>
+          <img src="${pokemonImage}" alt="${pokemonName}">
+        `;
+        pokemonStatus.innerHTML = statusContent;
+        pokemonStatus.classList.remove('d-none');
+      });
+    });
+}
+
 loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
 
-    const qtdRecordNextPage = offset + limit
+const qtdRecordNextPage = offset + limit
 
-    if (qtdRecordNextPage >= maxRecords){
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+if (qtdRecordNextPage >= maxRecords){
+    const newLimit = maxRecords - offset
+    loadPokemonItens(offset, newLimit)
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    }
-    else{
-        loadPokemonItens(offset, limit)
-    }
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    setTimeout(carregarPokemon, 500);
+}
+else{
+    loadPokemonItens(offset, limit)
+}
+
+    
 })
+  // Use setTimeout para atrasar a execução da função carregarPokemon
+  setTimeout(carregarPokemon, 500);
+
+
