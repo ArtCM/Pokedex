2b4b7327a335d43dd6e55ce84900b9f0dab2c469
Arthur <arthurperfilprof@hotmail.com>
2023-09-26 13:40:38 -0300

    concertado bug da janela de status do pokemon, faltando somente a parte do front

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,72 +5,83 @@ let offset = 0;
 
 const maxRecords = 151
 
+function renderPokemon(pokemon) {
+    return`
+        <li class="pokemon pokeclick ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+            </ol>
+            <img class="pokephoto" src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+        </li>
+    `;
+}
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => `
-                <li class="pokemon pokeclick ${pokemon.type}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-
-                    <div class="detail">
-                        <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                        </ol>
-                        <img class="pokephoto" src="${pokemon.photo}"
-                            alt="${pokemon.name}">
-                    </div>
-                </li>
-            `).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
-
-function carregarPokemon() {
-    const pokemonElements = document.querySelectorAll('.pokeclick');
-    pokemonElements.forEach((element) => {
-      element.addEventListener('click', () => {
-        const pokemonName = element.querySelector('.name').textContent;
-        const pokemonNumber = element.querySelector('.number').textContent;
-        const pokemonType = element.querySelector('.type').textContent;
-        const pokemonImage = element.querySelector('.pokephoto').src;
-  
-        const pokemonStatus = document.querySelector('.appearpoke');
-        const statusContent = `
-          <h2>${pokemonName}</h2>
-          <p>Número: ${pokemonNumber}</p>
-          <p>Tipo: ${pokemonType}</p>
-          <img src="${pokemonImage}" alt="${pokemonName}">
-        `;
-        pokemonStatus.innerHTML = statusContent;
-        pokemonStatus.classList.remove('d-none');
-      });
+        const newHtml = pokemons.map(renderPokemon).join("");
+        pokemonList.innerHTML += newHtml;
+        addPokemonClickHandlers();
     });
 }
 
-loadPokemonItens(offset, limit)
+function displayPokemonInfo(pokemon){
+    const pokemonStatus = document.querySelector(".appearpoke");
+    const statusContent = `
+        <h2>${pokemon.name}</h2>
+        <p>Número: ${pokemon.number}</p>
+        <p>Tipo: ${pokemon.type}</p>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+    `;
+    pokemonStatus.innerHTML = statusContent;
+    pokemonStatus.classList.remove("d-none");
+}
+
+function addPokemonClickHandlers() {
+    const pokemonElements = document.querySelectorAll(".pokeclick");
+    pokemonElements.forEach((element) => {
+        element.addEventListener("click", () => {
+            const pokemonName = element.querySelector(".name").textContent;
+            const pokemonNumber = element.querySelector(".number").textContent;
+            const pokemonType = element.querySelector(".type").textContent;
+            const pokemonImage = element.querySelector(".pokephoto").src;
+    
+            const pokemon = {
+                name: pokemonName,
+                number: pokemonNumber,
+                type: pokemonType,
+                photo: pokemonImage,
+            };
+    
+            displayPokemonInfo(pokemon);
+        });
+    });
+}
+
+window.addEventListener("load", () => {
+    loadPokemonItens(offset, limit);
+});
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
+    offset += limit;
+    const qtdRecordNextPage = offset + limit;
 
-const qtdRecordNextPage = offset + limit
+    if (qtdRecordNextPage >= maxRecords){
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
 
-if (qtdRecordNextPage >= maxRecords){
-    const newLimit = maxRecords - offset
-    loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    }
 
-    loadMoreButton.parentElement.removeChild(loadMoreButton)
-
-    setTimeout(carregarPokemon, 500);
-}
-else{
-    loadPokemonItens(offset, limit)
-}
-
+    else{
+        loadPokemonItens(offset, limit);
+    }
     
-})
-  // Use setTimeout para atrasar a execução da função carregarPokemon
-  setTimeout(carregarPokemon, 500);
+});
+
 
 
